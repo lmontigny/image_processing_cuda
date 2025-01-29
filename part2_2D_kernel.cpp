1c8285174e8881a4f30aaa3fdf9623274736afe6
Laurent Montigny <lmontigny@users.noreply.github.com>
2025-01-29 16:42:47 +0100

    Update part2_2D_kernel.cpp

--- part2_2D_kernel.cpp
+++ part2_2D_kernel.cpp
@@ -1,9 +1,33 @@
-__global__ void squareImage2D(float* image, int width, int height) {
+/*
+N=4 images
+NCHW channel first -> RGB, RGB, RGB, RGB
+NHWC channel last
+RBG, 3 matrix 
+
+img 3*1k*1k
+4 img 4*3*1k*1k
+
+b [0,3]
+z [0,11]
+c [0,2]
+------ ------ ------
+bacth index
+*/
+
+
+__global__ void squareImage2D(float* image, int width, int height, int dimz) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int z = blockIdx.z * blockDim.z + threadIdx.z;
 
-    if (x < width && y < height) {
-        int idx = y * width + x;
+    int b = z/3;
+    int c = z%3;
+
+    if (x < width && y < height && z < dimz) {
+        //int idx = y * width + x;
+        int idx = b*width*height*3;
+        int row = y * width * c;
+        int col = x * c + c;
         image[idx] = image[idx] * image[idx];
     }
 }
@@ -24,12 +48,14 @@ int main() {
     cudaMemcpy(d_image, h_image, width * height * sizeof(float), cudaMemcpyHostToDevice);
 
     // Kernel launch configuration
-    dim3 threadsPerBlock(16, 16);
+    dim3 threadsPerBlock(16, 16, 1);
+    int dimz = N*C;
     dim3 blocksPerGrid((width + threadsPerBlock.x - 1) / threadsPerBlock.x,
-                       (height + threadsPerBlock.y - 1) / threadsPerBlock.y);
+                       (height + threadsPerBlock.y - 1) / threadsPerBlock.y,
+                        dimz);
 
     // Launch the kernel
-    squareImage2D<<<blocksPerGrid, threadsPerBlock>>>(d_image, width, height);
+    squareImage2D<<<blocksPerGrid, threadsPerBlock>>>(d_image, width, height, dimz);
 
     // Copy result back to host
     cudaMemcpy(h_image, d_image, width * height * sizeof(float), cudaMemcpyDeviceToHost);
